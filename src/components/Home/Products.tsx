ae877985ac3114540edeabdf04658f2f23950514
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-12-28 04:43:55 +0100

    added stable flags for products availability

--- src/components/Home/Products.tsx
+++ src/components/Home/Products.tsx
@@ -9,6 +9,7 @@ import {
 const Products = () => {
   const products = [
     {
+      stable: true,
       icon: <PaymentIcon />,
       title: "Payment Link",
       description:
@@ -16,32 +17,36 @@ const Products = () => {
       buttonText: "Create Payment Link",
     },
     {
+      stable: false,
       icon: <InvoiceIcon />,
-      title: "Payment Link",
+      title: "Invoice",
       description:
         "Create a payment link in just a few clicks and share the link with your customers-no code required",
-      buttonText: "Create Payment Link",
+      buttonText: "Create and Send Invoice",
     },
     {
+      stable: false,
       icon: <WalletIcon />,
-      title: "Payment Link",
+      title: "Digital Wallet",
       description:
         "Create a payment link in just a few clicks and share the link with your customers-no code required",
-      buttonText: "Create Payment Link",
+      buttonText: "Create Wallet",
     },
     {
+      stable: false,
       icon: <P2pIcon />,
-      title: "Payment Link",
+      title: "Peer-to-Peer",
       description:
         "Create a payment link in just a few clicks and share the link with your customers-no code required",
-      buttonText: "Create Payment Link",
+      buttonText: "Peer to Peer Payment",
     },
     {
+      stable: false,
       icon: <TravelIcon />,
-      title: "Payment Link",
+      title: "Travels",
       description:
         "Create a payment link in just a few clicks and share the link with your customers-no code required",
-      buttonText: "Create Payment Link",
+      buttonText: "Travel Expense",
     },
   ];
 
@@ -52,23 +57,29 @@ const Products = () => {
       </h1>
 
       <div className="grid lg:grid-cols-3 mt-[16px] mb-[24px] gap-8">
-        {products.map((product) => (
-          <div className="h-[256px] rounded-[10px] gap-x-[16px] bg-[#F7F7F7] border border-[#E8EAED] shadow-balanceItem">
-            <div className="font-bold flex items-center gap-[12px] my-[16px]">
-              <div className="w-[41px] h-[41px] rounded-[12px] bg-productIconBackground">
+        {products.map((product, index) => (
+          <div
+            key={index}
+            className="h-[256px] rounded-[10px] gap-x-[16px] bg-white border border-[#E8EAED] shadow-balanceItem"
+          >
+            <div className="font-bold flex items-center gap-[12px] py-[16px] bg-[#F7F7F7]">
+              <div className="w-[41px] h-[41px] rounded-[12px] bg-productIconBackground flex justify-center items-center ml-[16px]">
                 {product.icon}
               </div>
               <div className="text-sm font-[700] text-[#0E0E0E] leading-[24px] tracking-[0.28px]">
                 {product.title}
               </div>
+              {!product.stable && (
+                <p className="text-[#008502] font-[500]">(Coming Soon)*</p>
+              )}
             </div>
 
-            <div className="bg-white">
-              <p className="text-sm font-[400] text-[#5E5E5E] leading-[24px] tracking-[0.28px]">
+            <div className="bg-white px-[16px]">
+              <p className="text-sm font-[400] text-[#5E5E5E] leading-[24px] tracking-[0.28px] pt-[9px]">
                 {product.description}
               </p>
-              <button className="text-white text-[16px] leading-[24px] tracking-[0.32px] font-medium bg-primary rounded-[10px] p-[10px] h-[48px] w-[149px] flex items-center justify-center">
-                Fund Balance
+              <button className="mt-[34px] text-[#0E0E0E] text-[14px] leading-[24px] tracking-[0.28px] font-medium bg-[#F7F7F7] border border-[#E8EAED] rounded-[10px] p-[10px] h-[44px] w-full flex items-center justify-center">
+                {product.buttonText}
               </button>
             </div>
           </div>
